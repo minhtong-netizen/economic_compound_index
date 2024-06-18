70c300a386249e2197da425461615c9a4bd5c2bc
minhtong-netizen <ducminhtong2k3@gmail.com>
2024-06-18 21:32:52 +0700

    feat: add more plot of relatedness matrix

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Hàm để lấy các ngành từ cột "Phân loại IPC"
 def extract_sectors(ipc_code):
@@ -12,3 +15,11 @@ def extract_sectors(ipc_code):
                 sectors_set.add(code[3])
 
     return sectors_set
+
+
+# Hàm vẽ heatmap
+def plot_heatmap(data, title):
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(data, annot=True, cmap='viridis', fmt='.2f', linewidths=.5)
+    plt.title(title)
+    plt.show()
